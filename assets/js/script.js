f5866b45d4022c33570d92c5e1a6fe71a6235522
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-04-19 10:49:15 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = 'zp390vJ7VO2N0j9EYkB1vEsKStE';
+const API_URL = 'https://ci-jshint.herokuapp.com/api';
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
